c00fc72f389fee2b69110f3c4e2209eccf477916
Jason Webb <jason.webb@rockfishinteractive.com>
2016-06-13 12:53:58 -0400

    Started adding in TeamCity apit changes for build queues

--- actions/lib/action.py
+++ actions/lib/action.py
@@ -2,4 +2,16 @@ from st2actions.runners.pythonrunner import Action
 
 import requests
 
-print "Hellow"
+class TeamCityAction(Action):
+    def __init__(self, config):
+        super(TeamCityAction, self).__init__(config)
+
+    def _get_client(self, repo=None):
+        if repo:
+            bb = Bitbucket(username=self.config['username'],
+                           password=self.config['password'],
+                           repo_name_or_slug=repo)
+        else:
+            bb = Bitbucket(username=self.config['email'],
+                           password=self.config['password'])
+        return bb

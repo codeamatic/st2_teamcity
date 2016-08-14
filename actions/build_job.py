fd81321e65411ac1031e1c415be0c42e83b10a8c
Jason Webb <jason.webb@rockfishinteractive.com>
2016-08-14 18:33:17 -0400

    Updated alias and action for initiating a build with a default branch

--- actions/build_job.py
+++ actions/build_job.py
@@ -3,10 +3,10 @@ from lib.action import TeamCityAction
 
 class BuildJob(TeamCityAction):
 
-    def run(self, build_config_id, vcs_branch_name="develop"):
+    def run(self, build_config_id, branch_name=None):
         request = '''<build branchName="%s">
                             <buildType id="%s"/>
-                            </build>''' % (vcs_branch_name, build_config_id)
+                            </build>''' % (branch_name, build_config_id)
 
         response = self._api_post('/buildQueue', request)
         return response
